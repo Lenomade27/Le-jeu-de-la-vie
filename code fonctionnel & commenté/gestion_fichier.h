8c86b38c7b21306c1d60ee406511feed7d7252fb
RayaneHassani <rayane.hassani@viacesi.fr>
2024-12-09 03:12:17 +0100

    Update gestion_fichier.h

--- code fonctionnel & commenté/gestion_fichier.h
+++ code fonctionnel & commenté/gestion_fichier.h
@@ -4,7 +4,7 @@ class gestion_fichier {
     protected:
         static int compteur_fichier ; // paramètre qui permet de calculer le nombre de fichiers créés et de les marquer
     public:
-    static void create_fichier(Grille* grille,string lien,string lien_nom) ; //méthode pour créer un fichier
+        static void create_fichier(Grille* grille,string lien,string lien_nom) ; //méthode pour créer un fichier
         static vector<vector<int>> get_fichier_data(string lien);  // méthode pour récupérer le contenu d'un fichier
         static string create_file(); // renvoie le lien d'un dossier créé
 };
